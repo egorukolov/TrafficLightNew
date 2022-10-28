72af3ac0633f9f8794ae5645db30dd1a79dde1d9
Egor Ukolov <ukolovegor@yandex.ru>
2022-10-28 21:07:09 +0700

    Настройка логики переключения цвета у светофора

--- TrafficLightNew/ViewController.swift
+++ TrafficLightNew/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red, yellow, green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    let lightTurnOn: CGFloat = 1
+    let lightTurnOff: CGFloat = 0.3
+    var currentLight = CurrentLight.red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSignalView.alpha = lightTurnOff
+        greenSignalView.alpha = lightTurnOff
+        yellowSignalView.alpha = lightTurnOff
+        startButton.layer.cornerRadius = 15
+        
     }
-
-
+    
+    @IBAction func startButtonPresswd() {
+        
+        startButton.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenSignalView.alpha = lightTurnOff
+            redSignalView.alpha = lightTurnOn
+            currentLight = .yellow
+        case .yellow:
+            redSignalView.alpha = lightTurnOff
+            yellowSignalView.alpha = lightTurnOn
+            currentLight = .green
+        case .green:
+            yellowSignalView.alpha = lightTurnOff
+            greenSignalView.alpha = lightTurnOn
+            currentLight = .red
+        }
+    }
 }
 
