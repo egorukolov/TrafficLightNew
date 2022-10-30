384a383fdfb883cce0bd40fff570378c2608e0d6
Egor Ukolov <ukolovegor@yandex.ru>
2022-10-30 21:23:05 +0700

    Ревью и округление вьюшек для светофора при запуске приложения

--- TrafficLightNew/ViewController.swift
+++ TrafficLightNew/ViewController.swift
@@ -16,19 +16,29 @@ class ViewController: UIViewController {
     @IBOutlet var redSignalView: UIView!
     @IBOutlet var greenSignalView: UIView!
     @IBOutlet var yellowSignalView: UIView!
+    
     @IBOutlet var startButton: UIButton!
     
-    let lightTurnOn: CGFloat = 1
-    let lightTurnOff: CGFloat = 0.3
-    var currentLight = CurrentLight.red
+    private let lightTurnOn: CGFloat = 1
+    private let lightTurnOff: CGFloat = 0.3
+    private var currentLight = CurrentLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         redSignalView.alpha = lightTurnOff
         greenSignalView.alpha = lightTurnOff
         yellowSignalView.alpha = lightTurnOff
+        
         startButton.layer.cornerRadius = 15
         
+    }
+    
+    override func viewWillLayoutSubviews() {
+        
+        redSignalView.layer.cornerRadius = redSignalView.frame.width / 2
+        yellowSignalView.layer.cornerRadius = yellowSignalView.frame.width / 2
+        greenSignalView.layer.cornerRadius = greenSignalView.frame.width / 2
     }
     
     @IBAction func startButtonPresswd() {
